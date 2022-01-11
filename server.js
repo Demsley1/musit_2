12dc4c72db48586be788863fcea3e6f9fdc85048
Denva <emsley3684@gmail.com>
2022-01-10 21:49:12 -0500

    server, database, and connection

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+const routes = require('./controllers');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const hbs = exphbs.create({ helpers });
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: process.env.SECRET,
+    cookie: { expires: (60*60*1000) },
+    store: new SequelizeStore({ db: sequelize }),
+    resave: false,
+    saveUninitialized: true
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess))
+
+app.use(routes);
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
