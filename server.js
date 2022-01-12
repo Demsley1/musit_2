a831fcae581fc815f6c17a2c6682497d714af249
Denva <emsley3684@gmail.com>
2022-01-12 18:18:39 -0500

    beginning homepage

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ const sess = {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session(sess))
+app.use(session(sess));
+
+app.use(routes);
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
