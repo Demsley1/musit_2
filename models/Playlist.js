360f60eaa449e33c19dc3999b5e5850cb9eb9089
Becca Byrne <aggiebec06@gmail.com>
2022-01-11 21:17:28 -0600

    create model for Playlist

---
+++ models/Playlist.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+// create our Playlist model
+class Playlist extends Model {}
+
+// create fields/columns for Post model
+Playlist.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    artist: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    song_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    genre: {
+        type: DataTypes.STRING,
+        allowNull: false,  
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post'
+  }
+);
+
+module.exports = Playlist;
