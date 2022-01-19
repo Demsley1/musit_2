2a56675e6aaf2d17111461f3ae40c871a4654f86
Brandon Diaz <brandonjustindiaz@yahoo.com>
2022-01-19 18:30:44 -0500

    I added the country music aswell because I forgot and updated the routes to fit the musicData seed

--- controllers/genre.js
+++ controllers/genre.js
@@ -1,11 +1,10 @@
 // importing the dependencies
 const router = require('express').Router();
-const { Playlist } = require('../models');
-const { sequelize } = require('../models/Playlist');
+const { Music } = require('../models');
 // all routes contain all the songs included in each genre
-// this is to get the Rock data
-router.get('/', (req, res) =>{
-    Playlist.findAll({
+// this is to get the Rock(#1) data
+router.get('/1', (req, res) =>{
+    Music.findAll({
         attributes: ['artist', 'song_title', 'genre'],
         where: {genre: 'Rock'}
     }).then( (data) =>{
@@ -17,23 +16,9 @@ router.get('/', (req, res) =>{
     });
 
 });
-// this is for the rap data
-router.get('/1', (req, res) =>{
-    Playlist.findAll({
-        attributes: ['artist', 'song_title', 'genre'],
-        where: {genre: 'Rap'}
-    }).then( (data) =>{
-        console.log(data)
-        res.render('./partials/genre')
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json(err)
-    });
-
-});
-// this is for the pop data
+// this is for the pop(#2) data
 router.get('/2', (req, res) =>{
-    Playlist.findAll({
+    Music.findAll({
         attributes: ['artist', 'song_title', 'genre'],
         where: {genre: 'Pop'}
     }).then( (data) =>{
@@ -45,9 +30,37 @@ router.get('/2', (req, res) =>{
     });
 
 });
-// lastly this is the route for the Other data
+// this is for the rap(hip-hop #3) data
 router.get('/3', (req, res) =>{
-    Playlist.findAll({
+    Music.findAll({
+        attributes: ['artist', 'song_title', 'genre'],
+        where: {genre: 'Rap'}
+    }).then( (data) =>{
+        console.log(data)
+        res.render('./partials/genre')
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    });
+
+});
+//this is the route for the Country(#4) data
+router.get('/4', (req, res) =>{
+    Music.findAll({
+        attributes: ['artist', 'song_title', 'genre'],
+        where: {genre: 'Country'}
+    }).then( (data) =>{
+        console.log(data)
+        res.render('./partials/genre')
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err)
+    });
+
+});
+// lastly this is the route for the Other(#5) data
+router.get('/5', (req, res) =>{
+    Music.findAll({
         attributes: ['artist', 'song_title', 'genre'],
         where: {genre: 'Other'}
     }).then( (data) =>{
