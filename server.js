2a9bb98429a8b16145f35f7f3b62f1f6990bed74
Becca Byrne <aggiebec06@gmail.com>
2022-01-18 22:27:58 -0600

    cleaned up playlist routes, JS, and handlebars

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -21,6 +20,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 app.use(routes);
 
